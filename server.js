f39a59624fed619f0da3560f6b062998a5e3307c
hurshedvosidov09 <hurshedvosidov09@gmail.com>
2026-05-20 19:51:27 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const fetch = require('node-fetch');
+const fetch = (...args) =>
+  import('node-fetch').then(({default: fetch}) => fetch(...args));;
 const serverless = require('serverless-http');
 
 dotenv.config();
